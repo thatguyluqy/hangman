8a68e7cb6371d7e3daee85317df2f9d9ce67073f
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-01 13:33:57 +0000

    milestone 2 task 3 & 4

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,23 @@
 #get a list of randon words
 #%%
 word_list = ["banana","pear","apple","grape","mango","watermelon","strawberry","lychee"]
-print(word_list)
-
-# %%
+#%%
 import random
 word = random.choice(word_list)
 print (word)
 
+#%%
+guess = input ("please enter a letter")
+if len(guess)==1 and str. isalpha(guess): 
+    print('good guess')
+else:
+     print ('Oops! That is not a valid input')
+#validate user input as single alphatic charachter
+#check user in put against word
+#retun correct and reveal word or incorrect and begin new list of guesses
+#show incorrect guesses
+#reveal all letters when all have been guessed
+#end game and congratulate user
 
 
 
