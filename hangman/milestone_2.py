12193532d715f6f38407d83f94b54d5877578e54
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-01 13:00:42 +0000

    completed milestone 2

---
+++ hangman/milestone_2.py
@@ -0,0 +1,29 @@
+#get a list of randon words
+#%%
+word_list = ["banana","pear","apple","grape","mango","watermelon","strawberry","lychee"]
+#randomly select a word
+
+import random
+word = random.choice(word_list)
+
+
+#convert the word in to asterisks
+
+
+#display and ask for user input
+#%%
+guess = input ("please enter a letter")
+if len(guess)==1 and str. isalpha(guess): 
+    print('good guess')
+else:
+     print ('Oops! That is not a valid input')
+#validate user input as single alphatic charachter
+#check user in put against word
+#retun correct and reveal word or incorrect and begin new list of guesses
+#show incorrect guesses
+#reveal all letters when all have been guessed
+#end game and congratulate user
+
+
+
+# %%
