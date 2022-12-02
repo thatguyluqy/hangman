ffea51b758340953e9d3587f4d3e3d96cd1531d2
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-02 00:40:54 +0000

    milestone 3 task 1 attempt 2

--- milestone_3.py
+++ milestone_3.py
@@ -2,10 +2,12 @@
 # looping code that keeps asking user for input and validating
 #must be one character long and a letter of the alphabet
 #%%
-guess = input("Enter your guess")
-while not (len(guess)==1 and str.isalpha(guess)):
-  guess = input("Invalid letter. Please, enter a single alphabetical character")
+guess = input("Enter your guess") 
+while len(guess) !=1 or guess.isalpha()==False:
+ guess = input("Invalid letter. Please, enter a single alphabetical character")
  
+
+
 
 
 
