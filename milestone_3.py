ee5b69695858a8f83785de4c4deed7acf3b27ad0
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-04 16:06:03 +0000

    milestone 3 task 2

--- milestone_3.py
+++ milestone_3.py
@@ -3,6 +3,12 @@
 #must be one character long and a letter of the alphabet
 
 #%%
+word_list = ["banana","pear","apple","grape","mango","watermelon","strawberry","lychee"]
+#randomly select a word
+
+import random
+word = random.choice(word_list)
+
 guess = ""
 
 while True:
@@ -15,6 +21,10 @@ while True:
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
         continue
+if guess in word :
+    print ( "Good guess! {guess} is in the word.")
+else:
+    print ("Sorry, {guess} is not in the word. Try again.")
 
 
 
