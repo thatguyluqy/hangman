9270f7d879180adad66dd34ea46369492583a97d
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-10 16:17:41 +0000

    reverted to re veryfy on portal

--- milestone_3.py
+++ milestone_3.py
@@ -1,41 +1,11 @@
-#milestone 3 leggo! 
-# looping code that keeps asking user for input and validating
-#must be one character long and a letter of the alphabet
+while True:
+    guess = input("Enter a single letter: ")
 
-#%%
-word_list = ["banana","pear","apple","grape","mango","watermelon","strawberry","lychee"]
-#randomly select a word
+    if len(guess) == 1 and guess.isalpha():
+        print(guess)
+        break
 
-import random
-word = random.choice(word_list)
-
-
-def ask_for_input():
-
-    guess = ""
-
-    while True:
-     guess = input(" Enter a single letter: ")
-
-     if len(guess) == 1 and guess.isalpha():
-         return guess
-
-     else:
-         print("Invalid letter. Please, enter a single alphabetical character.")
-         continue
-
-
-def check_guess(guess):
-    if guess in word :
-     print (f'Good guess! {guess} is in the word.')
-     
     else:
-     print (f'Sorry, {guess} is not in the word. Try again.')
+        print("Invalid letter. Please, enter a single alphabetical character.")
+        continue
 
-
-guess = ask_for_input()
-guess = guess.lower()
-check_guess(guess)
-
-
-# %%
