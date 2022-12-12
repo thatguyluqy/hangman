a959e4b641bfc71b402bd3e65f80b358f33e7d66
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-12 18:09:09 +0000

    alligned with feedback

--- milestone_4.py
+++ milestone_4.py
@@ -7,7 +7,7 @@ class Hangman:
         self.list_of_guesses = []
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word_guessed = self.word.replace(self.word[0:], "\"-\","* (len(self.word)))
+        self.word_guessed = self.word.replace(self.word[0:], "_"* (len(self.word)))
         self.num_list = "".join(set(self.word)).lower()
 
     
