e77d8f5b57f7b95c5d4d136dacb0009b4fb76449
Luqman Khalifa <you@example.comluqman.khalifa@gmail.com>
2022-12-12 17:01:19 +0000

    milestone 4 task 2

--- milestone_4.py
+++ milestone_4.py
@@ -9,3 +9,24 @@ class Hangman:
         self.num_lives = num_lives
         self.word_guessed = self.word.replace(self.word[0:], "\"-\","* (len(self.word)))
         self.num_list = "".join(set(self.word)).lower()
+    
+    def ask_for_input(self):
+        guess = ""
+        while True:
+            self.guess = input(" Enter a single letter: ")
+            if len(guess) == 1 and guess.isalpha():
+                 return guess
+            elif guess in self.list_of_guesses:
+                 print ("You already tried that letter!.")
+            else:
+                 print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    def check_guess(self):
+        if self.guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print()
+
+
+game2 = Hangman(["banana","pear","apple","grape","mango","watermelon","strawberry","lychee"])
+game2.ask_for_input()
